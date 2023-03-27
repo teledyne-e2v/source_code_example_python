2e53734e972cf3a745d7a14472516ae56c61cd19
lchevallier-teledyne <124684007+lchevallier-teledyne@users.noreply.github.com>
2023-03-27 09:45:05 +0200

    update with frequency and change mode to BOARD

--- GPIO_example.py
+++ GPIO_example.py
@@ -4,24 +4,29 @@
 Created on Mon Mar 27 09:13:58 2023
 
 @author: teledyne
+
+This code show how to use GPIO on Jetson Nano
 """
 
 import RPi.GPIO as GPIO
 import time
 
-output_pin = 18  # BCM pin 18, BOARD pin 12
+output_pin = 12  # BOARD pin 12
 
 def main():
     # Pin Setup:
-    GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
+    GPIO.setmode(GPIO.BOARD)
     # set pin as an output pin with optional initial state of HIGH
     GPIO.setup(output_pin, GPIO.OUT)   # you can set initial state with the argument : initial=GPIO.HIGH
 
     print("Starting demo now! Press CTRL+C to exit")
     tension = GPIO.HIGH
+    
+    frequency=10
+    
     try:
-        while True:
-            time.sleep(0.5)
+        while True: 
+            time.sleep(1/(2*frequency)) #With this sleep the frequency should be 1Hz
             GPIO.output(output_pin, tension)
             tension ^= GPIO.HIGH # set to opposit value.
     finally:
@@ -29,3 +34,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
